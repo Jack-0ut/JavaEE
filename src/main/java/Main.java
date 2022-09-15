37c85e7938a8ceef9b114422b7bad7c651f73227
Jack-0ut <yugin12s@gmail.com>
2022-09-15 20:53:37 +0300

    Showing how it works using example where we check if Employee is already in the list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
+import Exceptions.EmployeeRegistryException;
 import Exceptions.FieldLengthLimitException;
 import Exceptions.IncorrectSalaryException;
 
 public class Main {
     public static void main(String[] args) {
-
         try{
-            Employee employee = new Employee("Eugeneeeeeruiuuvgg","Solhhhhhhhukukkukytrtgrggfgfdgovey",-12500);
+            Manager manager = new Manager("Forrest Gump","HR",500000,2000);
+            Employee employee1 = new Employee("Eugene Solovey","Finance",125000,manager.getId());
+            Employee employee2 = new Employee("Alex Bergman","Finance",150000,manager.getId());
+            Employee employee3 =  new Employee("Alex Bergman","Finance",150000,manager.getId());
+
+            Registry registry = Registry.getRegistry();
+            registry.addEmployee(manager);
+            registry.addEmployee(employee1);
+            registry.addEmployee(employee2);
+            registry.addEmployee(employee2);
+            registry.printList();
         } catch (IncorrectSalaryException e) {
             System.out.println(e.getMessage());
         } catch (FieldLengthLimitException e) {
